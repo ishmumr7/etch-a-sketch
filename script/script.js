2e92d0f9cfed3229a28586ac847a88b0e9261bcb
Tahzeeb <ishmum20000@gmail.com>
2022-08-15 19:10:00 +0600

    Add changeGrid and clear

--- script/script.js
+++ script/script.js
@@ -6,6 +6,12 @@ let size = DEFAULT_SIZE;
 let color = DEFAULT_COLOR;
 let mode = DEFAULT_MODE;
 
+let clear = document.querySelector('#clear');
+let gridSize = document.querySelector('#grid-size');
+
+clear.addEventListener('click', clearGrid)
+gridSize.addEventListener('click', changeSize);
+
 let body = document.querySelector('.body');
 
 let isClicked = false;
@@ -21,6 +27,7 @@ function createGrid(size) {
     let sketchpad = document.createElement('div');
     sketchpad.setAttribute('class', 'sketchpad');
     sketchpad.setAttribute('style', `grid-template-columns: repeat(${size}, 1fr)`);
+    // sketchpad.setAttribute('style', `grid-template-rows: repeat(${size}, 1fr)`);
 
     for (let i = 0; i < (size * size); i++) {
         let pixel = document.createElement('div');
@@ -41,6 +48,36 @@ function paint(e) {
         this.style.backgroundColor = color;
     }
     
+}
+
+function emptyGrid() {
+    let container = document.querySelector('.sketchpad-container');
+    container.innerText = '';
+}
+
+function clearGrid() {
+    let grids = document.querySelectorAll('.sketchpad .pixel');
+    grids.forEach(grid => {
+        grid.style.backgroundColor = 'white';
+    });
+}
+
+function changeSize() {
+    let input;
+    while (true) {
+        input = prompt('Enter number of squares: ');
+        if (isNaN(input)) {
+            alert("Not a number!");
+        } else if (input > 0 && input <= 100) {
+            size = parseInt(input);
+            break;
+        } else {
+            alert("Number must be between 1 and 100!");
+        }
+    }
+    console.log(size);
+    emptyGrid();
+    createGrid(size);
 }
 
 createGrid(size);
